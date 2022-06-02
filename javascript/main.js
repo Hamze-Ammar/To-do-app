6decfe26b3300f207d59f5776f25284f4c0c8898
Hamze-A <99362310+Hamze-A@users.noreply.github.com>
2022-06-02 20:25:03 +0300

    Added editTask function

--- javascript/main.js
+++ javascript/main.js
@@ -87,15 +87,15 @@ function createTask(){
 function displayTasksToDo(new_task){
     // Get the element
     let tasks_to_do = $("#tasks-to-do");
-    let new_task_element = `<div class="task-box-header row">
-                        <span class="row-id" >${new_task.id}</span>
-                        <span class="row-Created">${new_task.date}</span>
-                        <span class="row-Title">${new_task.title}</span>
-                        <span class="row-Description">${new_task.description}</span>
-                        <span class="row-Point">${new_task.point}</span>
-                        <span class="row-Due">${new_task.due_date}</span>
-                        <span class="row-done">${new_task.is_done}</span>
-                        <span class="row-Options"><i onClick="markAsDone(this)" id="fa-check" class="fa-solid fa-check"></i><i class="fa-solid fa-pen"></i><i onClick="deletePost(this)" class="fa-solid fa-trash-can"></i></span>
+    let new_task_element = `<div class="task-box-header row row-content-to-do">
+                        <span class="row-id " >${new_task.id}</span>
+                        <span class="row-Created ">${new_task.date}</span>
+                        <span class="row-Title ">${new_task.title}</span>
+                        <span class="row-Description ">${new_task.description}</span>
+                        <span class="row-Point ">${new_task.point}</span>
+                        <span class="row-Due ">${new_task.due_date}</span>
+                        <span class="row-done ">${new_task.is_done}</span>
+                        <span class="row-Options "><i onClick="markAsDone(this)" id="fa-check" class="fa-solid fa-check"></i><i onClick="editTask(this)" class="fa-solid fa-pen" id="edit-task-pen"></i><i onClick="deletePost(this)" class="fa-solid fa-trash-can"></i></span>
                     </div>`
     tasks_to_do.append(new_task_element);
 }
@@ -215,7 +215,7 @@ function displayCompletedTask(task){
     let is_done = true;
     // Get the element
     let task_completed = $("#tasks-completed");
-    let new_task_element = `<div class="task-box-header row">
+    let new_task_element = `<div class="task-box-header row ">
                         <span class="row-id" >${task.id}</span>
                         <span class="row-Created">${task.date}</span>
                         <span class="row-Title">${task.title}</span>
@@ -254,4 +254,64 @@ function deleteFromLocalStorage(target_id){
             //return removed_task;
         }
     }
+}
+
+//============== Sort by point =======================
+
+let sort_by_point = false;
+$("#sort-by-point").click(function(){
+    // First lets empty the table
+    $("div").remove(".row-content-to-do");
+    // get tasks to do from local storage
+    const tasks_to_do = JSON.parse(localStorage.getItem("tasks_to_do"));
+    sorted_array = [];
+    tasks_to_do.sort(compare);
+    //console.log(tasks_to_do);
+
+    for (let i=0; i<tasks_to_do.length; i++){
+        displayTasksToDo(tasks_to_do[i]);
+    }
+    
+})
+
+// The sort function
+function compare( a, b ) {
+    if ( a.point < b.point ){
+      return 1;
+    }
+    if ( a.point > b.point ){
+      return -1;
+    }
+    return 0;
+  }
+
+// Sort by date again? simply refresh the page
+$("#sort-by-date").click(function(){
+    location.reload();
+})
+
+//============== Edit Tasks =======================
+
+let editTask = (e) => {
+    //get the row info
+    let selectedTask = e.parentElement.parentElement;
+
+    let id = selectedTask.children[0].innerHTML;
+    let date = selectedTask.children[1].innerHTML;
+    let title = selectedTask.children[2].innerHTML;
+    let description = selectedTask.children[3].innerHTML;
+    let point = selectedTask.children[4].innerHTML;
+    let due_date =  selectedTask.children[5].innerHTML;
+    let is_done = selectedTask.children[6].innerHTML;
+    
+    // Trigger a click to open modal
+    $("#myBtn").trigger("click");
+    //fill the modal input with those values
+    $("#modal-title").val(title);
+    $("#modal-description").val(description);
+    $("#modal-point").val(point);
+    $("#modal-due-date").val(due_date);
+
+    selectedTask.remove();
+    removeTaskFromStorage(id);
 }
